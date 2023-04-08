431ad8757a644b5d69b8cd278f3042e3654dd800
Steve Romero <romero.steve.a@gmail.com>
2023-04-07 18:24:19 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const m = document.getElementById('main');
+m.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Steve is the champion";
+
+document.body.append(newHeader);
+
+
